6c63dd1be8e652f53670ac0b6e7596662f8bd225
anwar@LAPTOP-E3DEI6E7 <laptop-e3dei6e7\anwar>
2024-12-03 22:05:40 +0100

    rating count for each movie

---
+++ hadoop/user_rating_fromme.py
@@ -0,0 +1,24 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+
+class CountRating(MRJob):
+    
+    def steps(self):
+        return [
+            MRStep(
+                mapper= self.mapper_most_rating,
+                reducer = self.reducer_most_rating
+            )
+        ]
+
+    def mapper_most_rating(self,_, line):
+        (userID, movieID, rating, timestamp) = line.split("\t")
+        yield movieID, int(rating)
+    
+    def reducer_most_rating(self,key, value):
+        yield key , sum(value)
+    
+
+if __name__ == '__main__':
+    CountRating.run()
